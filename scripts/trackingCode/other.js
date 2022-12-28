8439b0d3ec043b9af35c5e8da31b3c3a9ba3ffef
Елизавета Лисенкова <eelisn@MacBook-Air-Elizaveta-3.local>
2022-12-28 05:07:31 +0300

    fixed bugs

---
+++ scripts/trackingCode/other.js
@@ -0,0 +1,11 @@
+
+window.gdeslon_q = window.gdeslon_q || [];
+window.gdeslon_q.push({
+    page_type: "other", //тип страницы: main, list, card, basket, thanks, other
+    merchant_id: "107016", //id оффера в нашей системе
+    order_id: "", //id заказа
+    category_id: "", //id текущей категории
+    products: "",
+    deduplication: "gdeslon", //параметр дедупликации заказов (динамическое значение)
+    user_id: "" //идентификатор пользователя
+});
