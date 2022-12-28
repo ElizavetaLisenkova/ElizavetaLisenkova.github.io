f7f3c5c2bf383aece1b42e229febf804b1a572c0
Елизавета Лисенкова <eelisn@MacBook-Air-Elizaveta-3.local>
2022-12-28 05:19:44 +0300

    add tracking code

--- scripts/trackingCode/card.js
+++ scripts/trackingCode/card.js
@@ -1,7 +1,7 @@
 
 window.gdeslon_q = window.gdeslon_q || [];
 window.gdeslon_q.push({
-    page_type: "main", //тип страницы: main, list, card, basket, thanks, other
+    page_type: "card", //тип страницы: main, list, card, basket, thanks, other
     merchant_id: "107016", //id оффера в нашей системе
     order_id: "", //id заказа
     category_id: "", //id текущей категории
