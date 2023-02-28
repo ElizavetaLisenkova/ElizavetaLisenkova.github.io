454db86d5c5fc2518c55cc9cc19f8c1521c33e57
Елизавета Лисенкова <eelisn@MacBook-Air-Elizaveta-3.local>
2023-02-28 08:29:28 +0300

    add deduplication code

---
+++ scripts/attribution.js
@@ -0,0 +1,28 @@
+function getParam(key) {
+    var p = window.location.search;
+    p = p.match(new RegExp(key + '=([^&=]+)'));
+    return p ? p[1] : false;
+};
+
+function eraseCookie(name) {   
+    document.cookie = name+'=; Max-Age=-99999999;';  
+};
+
+source = getParam('utm_source');
+
+if (source != 'gdeslon') {
+    eraseCookie("gdeslon.ru.__arc_aid");
+}
+
+cookieName = "gdeslon.ru.__arc_aid";
+
+function checkCurrentDeduplication(cookieName) {
+    if (document.cookie.indexOf(cookieName) == 0) {
+        localStorage.setItem("deduplication", "gdeslon");
+        
+    }else{
+        localStorage.setItem("deduplication", "other");
+    }
+}
+
+checkCurrentDeduplication(cookieName);
--- scripts/trackingCode/main.js
+++ scripts/trackingCode/main.js
@@ -6,6 +6,6 @@ window.gdeslon_q.push({
     order_id: "", //id заказа
     category_id: "", //id текущей категории
     products: [],
-    deduplication: "gdeslon", //параметр дедупликации заказов (динамическое значение)
+    deduplication: localStorage.getItem("deduplication"), //параметр дедупликации заказов (динамическое значение)
     user_id: "" //идентификатор пользователя
 });
