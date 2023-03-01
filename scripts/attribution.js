ac628428ef164ba3aaaaeedfb2afe8b2a1f11361
Елизавета Лисенкова <eelisn@MacBook-Air-Elizaveta-3.local>
2023-03-01 18:08:48 +0300

    не находит сорс в аттрибуции, попытка починить 1.0

--- scripts/attribution.js
+++ scripts/attribution.js
@@ -1,20 +1,13 @@
 function getParam(key) {
     var p = window.location.search;
     p = p.match(new RegExp(key + '=([^&=]+)'));
+
     return p ? p[1] : false;
 };
 
 function eraseCookie(name) {   
     document.cookie = name+'=; Max-Age=-99999999;';  
 };
-
-source = getParam('utm_source');
-
-if (source != 'gdeslon') {
-    eraseCookie("gdeslon.ru.__arc_aid");
-}
-
-cookieName = "gdeslon.ru.__arc_aid";
 
 function checkCurrentDeduplication(cookieName) {
     if (document.cookie.indexOf(cookieName) == 0) {
@@ -23,6 +16,15 @@ function checkCurrentDeduplication(cookieName) {
     }else{
         localStorage.setItem("deduplication", "other");
     }
+}
+
+
+cookieName = "gdeslon.ru.__arc_aid";
+source = getParam('utm_source');
+console.log(source)
+
+if (source != 'gdeslon') {
+    eraseCookie(cookieName);
 }
 
 checkCurrentDeduplication(cookieName);
